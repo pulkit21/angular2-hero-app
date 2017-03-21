dca54715be19c80ef5a1e5e0bc89a7c9e2f13dd3
Pulkit Agarwal <pulkit.ag02@gmail.com>
2017-03-21 12:32:15 +0530

    Listing of heroes

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
   selector: 'app-hero',
@@ -12,10 +13,17 @@ export class HeroComponent implements OnInit {
   }
 
   title = 'Tour of Heroes';
-  // hero = 'Windstrom';
+  heroes: Hero[] = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+  ];
 
-  hero : Hero = {
-    id: 1,
-    name: 'Pulkit'
-  };
 }
